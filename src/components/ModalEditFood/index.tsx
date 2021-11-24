e7626b8afcfe9435356f0896816567728113c10d
Gustavo Mendes <gustavomendes2112@hotmail.com>
2021-11-24 20:49:18 -0300

    modalEditFood finalizado

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -1,9 +1,11 @@
-import { FiCheckSquare } from 'react-icons/fi';
+import { useRef } from "react"
+import { FiCheckSquare } from "react-icons/fi"
 
 import { Form } from './styles';
-import { Modal } from '../Modal';
-import Input from '../Input';
-import { useRef } from 'react';
+import { Modal } from "../Modal"
+import Input from "../Input"
+
+
 
 interface Food {
   image: string;
@@ -13,32 +15,34 @@ interface Food {
 }
 
 
-interface ModalAddFoodProps {
+interface ModalEditFoodProps {
   isOpen: boolean;
   setIsOpen: () => void;
-  handleAddFood: (food: Food) => void
+  handleUpdateFood: (food: Food) => void;
+  editingFood: Food
 }
 
-export function ModalAddFood({ handleAddFood, isOpen, setIsOpen }:ModalAddFoodProps) {
+export function ModalEditFood({ handleUpdateFood, isOpen, setIsOpen, editingFood }: ModalEditFoodProps) {
   const formRef = useRef(null)
   
-  async function handleSubmit (data: Food) {
-    await handleAddFood(data)
+  async function handleSubmit(data:Food) {
+    handleUpdateFood(data)
     setIsOpen()
   }
   
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Novo Prato</h1>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
         <Input name="image" placeholder="Cole o link aqui" icon={undefined} />
 
         <Input name="name" placeholder="Ex: Moda Italiana" icon={undefined} />
         <Input name="price" placeholder="Ex: 19.90" icon={undefined} />
 
         <Input name="description" placeholder="Descrição" icon={undefined} />
-        <button type="submit" data-testid="add-food-button">
-          <p className="text">Adicionar Prato</p>
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
